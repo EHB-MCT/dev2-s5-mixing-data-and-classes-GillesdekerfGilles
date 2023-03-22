531407fd19f4816bd1aee4d8ebb5208974ce9211
Gilles De Kerf <Gilles.De.kerf@student.ehb.be>
2023-03-22 12:26:55 +0100

    opdracht jokes

--- js/index.js
+++ js/index.js
@@ -1,14 +1,38 @@
 "use strict";
-
+import Joke from "./joke.js";
 const app = {
     items: [],
     init() {
+        this.getData();
     },
     getData() {
+
+        fetch('https://v2.jokeapi.dev/joke/Dark?blacklistFlags=sexist&type=single&amount=3')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                /*       console.log(data.jokes[1].joke); */
+                /*   for (let i = 0; i < data.jokes.length; i++) {
+                      console.log(data.jokes[i].joke);
+                      
+                  } */
+                console.log(data.Jokes);
+                data.jokes.forEach(function (j) {
+                    const joke = new Joke(j.setup, j.delivery);
+                    app.items.push(joke)
+                })
+            });
+
+
+
+
     },
-    onSearch() {
-    },
+    onSearch() {},
     render(message) {
+        let htmlString = ``
+
+        /*         document.querySelector('#messageContainer').insertAdjacentHTML("beforeend", htmlString) */
     }
 
 }
